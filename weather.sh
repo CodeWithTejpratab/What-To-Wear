a72cefcceb05768497064d9ae726d9296a38ee07
Rahul Ramjeawon <96823012+CodeWithTejpratab@users.noreply.github.com>
2023-11-11 13:04:50 -0500

    Add files via upload

---
+++ weather.sh
@@ -0,0 +1,32 @@
+#!/bin/zsh
+
+source ~/.zshrc
+
+# OpenWeatherMap API endpoint
+API_ENDPOINT="http://api.openweathermap.org/data/2.5/weather?q=${CITY},${COUNTRY}&appid=${API_KEY}"
+
+# Get weather information using curl and jq
+WEATHER_TEMP=$(curl -s "$API_ENDPOINT" | jq -r '.main.temp')
+
+# Convert temperature from Kelvin to Fahrenheit
+WEATHER_TEMP_F=$(echo "scale=2; (${WEATHER_TEMP} * 9/5) - 459.67" | bc)
+
+# Phone number to send the message
+PHONE_NUMBER="$1"
+
+if [[ $WEATER_TEMP_F -ge 60 ]]
+then 
+	MESSAGE="Current temperature in ${CITY}: ${WEATHER_TEMP_F}°F. You should wear something light as it is hot today"
+elif [[ $WEATHER_TEMP_F -ge 50 ]] && [[ $WEATHER_TEMP_F -le 60 ]]
+then 
+	MESSAGE="Current temperature in ${CITY}: ${WEATHER_TEMP_F}°F. You should wear something cozy today, such as a sweater as it is a bit chilly today"
+elif [[ $WEATHER_TEMP_F -lt 50 ]]
+then	
+	MESSAGE="Current temperature in ${CITY}: ${WEATHER_TEMP_F}°F. You should wear a heavy Jacket as it is very cold today, maybe some gloves"
+fi
+
+osascript -e "tell application \"Messages\"
+  set targetService to 1st service whose service type = iMessage
+  set targetBuddy to buddy \"${PHONE_NUMBER}\" of targetService
+  send \"${MESSAGE}\" to targetBuddy
+end tell"
